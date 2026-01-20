71c2985fba15fb5723b49e0b84ac6a5655ac82a6
banavasi <shashank.v.shandilya@gmail.com>
2026-01-19 20:56:02 -0700

    init new astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
+
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  output: "static",
-  integrations: [mdx(), sitemap(), react()],
   vite: {
-    plugins: [tailwindcss()],
-  },
+      plugins: [tailwindcss()],
+	},
+
+  integrations: [react()],
 });
--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Shashank Shandilya";
-export const SITE_DESCRIPTION = "Senior Technical Lead & Solutions Architect. Transforming business challenges into scalable, intelligent digital ecosystems.";
